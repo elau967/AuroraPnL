2ca1ebc893d6bba7699d8fd070fcc3b144bb636d
Eric Lau <201292700+elau967@users.noreply.github.com>
2025-03-15 18:31:12 -0400

    1. Import packages 2. Add an upload_csv function that will calculate, display, and return the dataframe

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from aurorapnl!")
+import pandas as pd
+import streamlit as st
+import yfinance as yf
 
+# Prompt a user to upload a CSV file
+def upload_csv():
+    uploadedFile = st.file_uploader("Upload your CSV file", type = "csv")
+
+    # If a CSV file is received, read it and make a "Total Value" column
+    if uploadedFile:
+        df = pd.read_csv(uploadedFile, dtype = {"Ticker": str, "Cost Basis": float, "Amount of Shares": float})
+        df["Total Value"] = df["Cost Basis"] * df["Amount of Shares"]
+
+        # Combine duplicate ticker names, sum columns, and calculate new cost basis
+        df = df.groupby("Ticker").agg({
+            "Total Value": "sum",
+            "Amount of Shares": "sum"
+        }).reset_index()
+        df["Cost Basis"] = df["Total Value"] / df["Amount of Shares"]
+
+        # Display and return the dataframe
+        st.write(df)
+        return df
+
+def main():
+    st.write("Aurora PnL")
+    df = upload_csv()
 
 if __name__ == "__main__":
     main()
